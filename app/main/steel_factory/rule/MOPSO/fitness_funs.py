361b1f2e711525649fe97ce33c3d0bd53cbffa51
9Echo <1094015147@qq.com>
2021-04-28 14:22:28 +0800

    update

---
+++ app/main/steel_factory/rule/MOPSO/fitness_funs.py
@@ -0,0 +1,18 @@
+# 作者： pingyu
+# 日期： 2021/4/18
+# 时间： 14:13   
+
+import numpy as np
+
+#为了便于图示观察，试验测试函数为二维输入、二维输出
+#适应值函数：实际使用时请根据具体应用背景自定义
+
+
+def fitness_(in_):
+    degree_45 = ((in_[0]-in_[1])**2/2)**0.5
+    degree_135 = ((in_[0]+in_[1])**2/2)**0.5
+    fit_1 = 1 - np.exp(-degree_45 ** 2 / 0.5) * np.exp(-(degree_135 - np.sqrt(200)) ** 2 / 250)
+    fit_2 = 1 - np.exp(-degree_45 ** 2 / 5) * np.exp(-degree_135 ** 2 / 350)
+
+    return [fit_1, fit_2]
+
